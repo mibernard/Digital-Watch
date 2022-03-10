645c0935a08012da0af6da99300a9f201c7e2f6d
Matthew <matthewbernard4@gmail.com>
2022-03-09 16:04:18 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from "react";
+import "./App.css";
+import ContactApp from "./components/ContactApp";
+
+let App = () => {
+  return (
+    <Fragment>
+      <ContactApp />
+    </Fragment>
+  );
+};
+
+export default App;
