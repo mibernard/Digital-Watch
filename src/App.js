72f4a929931427f9834631e635e101d4619b2514
Matthew <matthewbernard4@gmail.com>
2022-03-09 16:37:58 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React, { Fragment } from "react";
 import "./App.css";
-import ContactApp from "./components/ContactApp";
+import DigitalWatch from "./components/DigitalWatch";
 
 let App = () => {
+  //or function App() or  let App = function()
   return (
     <Fragment>
-      <ContactApp />
+      <nav className="navbar navbar-dark bg-dark navbar-expand-sm">
+        <a href="/" className="navbar-brand">
+          React with LifeCycle Methods
+        </a>
+      </nav>
+      <DigitalWatch />
     </Fragment>
   );
 };
